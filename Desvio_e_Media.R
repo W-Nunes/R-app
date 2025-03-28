45fd2d9439c811654a3e58198b07d83392ca3c2c
W-Nunes <renanwn96@gmail.com>
2025-03-27 21:56:31 -0300

    Update Desvio_e_Media.R
    Troca do comentário

--- Desvio_e_Media.R
+++ Desvio_e_Media.R
@@ -3,12 +3,12 @@
 culturas = data.frame(nome = c("Milho", "Soja"), altura = c(3000, 3000), largura = c(8000, 3000), area = c(24000000, 9000000))
 
 
-## A média das áreas das culturas em metros será:
+## A média das áreas das culturas em metros é:
 
 media = mean(culturas$area)
 print(media)
 
-## O desvio padrão das áreas das culturas em metros será:
+## O desvio padrão das áreas das culturas em metros é:
 
 desvio = sd(culturas$area)
 print(desvio)
